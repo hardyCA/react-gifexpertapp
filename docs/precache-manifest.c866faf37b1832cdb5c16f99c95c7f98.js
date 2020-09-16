1b63ce790dc098105fbb98fa67e00b267d1c786b
hardy <hardy_0002009@hotmail.com>
2020-09-16 16:56:15 +0200

    docs agregar

---
+++ docs/precache-manifest.c866faf37b1832cdb5c16f99c95c7f98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e69578fcaa362f0f43c53771a83dfea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73bfaaad325ea861b57c",
+    "url": "/static/css/main.86539281.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73bfaaad325ea861b57c",
+    "url": "/static/js/main.42e7f0ce.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
